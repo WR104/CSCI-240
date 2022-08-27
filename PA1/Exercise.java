a380aae3aebdce781df3ce8c6271bd853a602f17
WR104 <252600486L@gmail.com>
2022-08-27 16:54:34 -0700

    add template

--- PA1/Exercise.java
+++ PA1/Exercise.java
@@ -1,4 +1,15 @@
+
+
 package PA1;
 
 public class Exercise {
+    public Boolean exercise1 (int[] nums){
+        for(int i=0;i<nums.length-1;i++){
+            for(int j=i+1;j<nums.length;j++){
+                if(nums[i] == nums[j])
+                    return false;
+            }
+        }
+        return true;
+    }
 }
--- PA1/Test.java
+++ PA1/Test.java
@@ -2,6 +2,9 @@ package PA1;
 
 public class Test {
     public static void main(String[] args){
-        System.out.println("Hello world!");
+        int[] nums1 = {5,7,2,6,4};
+        int[] nums2 = {5,7,2,6,4,2};
+        System.out.println(new Exercise().exercise1(nums1));
+        System.out.println(new Exercise().exercise1(nums2));
     }
 }
