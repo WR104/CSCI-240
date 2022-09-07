715447a22153ee4b72755987962999deea953076
WR104 <252600486L@gmail.com>
2022-09-07 01:31:48 -0700

    add PA1

--- PA1/Test.java
+++ PA1/Test.java
@@ -2,12 +2,8 @@ package PA1;
 
 public class Test {
     public static void main(String[] args){
-        GameScore g1 = new GameScore("Classic Pac-Man");
-        g1.add(new MyGameEntry("Jill",980,"08/05/2021"));
-        g1.add(new MyGameEntry("Jack",600,"08/18/2021"));
-        g1.add(new MyGameEntry("Rob",875,"07/30/2021"));
-        g1.add(new MyGameEntry("Rob" ,900,"08/02/2021"));
-        g1.print();
+        System.out.println(new Exercise().EC1(3));
+        System.out.println(new Exercise().EC1(20));
 
     }
 }
