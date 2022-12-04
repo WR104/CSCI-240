c12ce5f073828894353cb1c81966c8686c4416be
WR104 <252600486L@gmail.com>
2022-12-03 22:07:31 -0800

    add make up assignment

--- Makeup/FindInversions.java
+++ Makeup/FindInversions.java
@@ -1,4 +1,51 @@
+package Makeup;
+
+import java.util.Arrays;
+
 public class FindInversions {
 
-    public int 
+    public static int method1 (int[] arr){
+        int count = 0;
+        for(int i = 0; i < arr.length - 1; i++){
+            for(int j = i + 1; j < arr.length; j++){
+                if(arr[i] > arr[j])
+                    count += 1;
+            }
+        }
+        return count;
+    }
+
+    public static int method2(int[] arr) {
+        if (arr.length < 2)
+            return 0;
+
+        int m = (arr.length + 1) / 2;
+        int left[] = Arrays.copyOfRange(arr, 0, m);
+        int right[] = Arrays.copyOfRange(arr, m, arr.length);
+
+        return method2(left) + method1(right) + merge(arr, left, right);
+    }
+
+    private static int merge(int[] arr, int[] left, int[] right) {
+        int i = 0, j = 0;
+        int count = 0;
+        while (i < left.length || j < right.length) {
+            if (i == left.length) {
+                arr[i+j] = right[j];
+                j += 1;
+            } else if (j == right.length) {
+                arr[i+j] = left[i];
+                i += 1;
+            } else if (left[i] <= right[j]) {
+                arr[i+j] = left[i];
+                i += 1;
+            } else {
+                arr[i+j] = right[j];
+                count += left.length-i;
+                j += 1;
+            }
+        }
+        return count;
+    }
+
 }
--- Makeup/Test.java
+++ Makeup/Test.java
@@ -1,2 +1,12 @@
-package Makeup;public class Test {
+package Makeup;
+
+import java.io.IOException;
+
+public class Test {
+    public static void main(String[] args) throws IOException {
+        int[] arr1 = {1, 9, 6, 4, 5};
+        int[] arr2 = {1, 4, 5, 6, 9};
+
+        System.out.println(FindInversions.method1(arr2));
+    }
 }
